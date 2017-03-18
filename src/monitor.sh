7db392bae9be8cb82abfd2e751ee4b5dd0a594bf
Your Name <you@example.com>
2017-03-18 13:47:31 -0600

    creacion del script monitoreo

---
+++ src/monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Programa para realizar respaldos de documentacion
+
+current_pwd=`pwd`
+
+
+
+echo Reporte:  >> /tmp/reporte
+date >> /tmp/reporte
+pwd >> /tmp/reporte
+#du- sh >> /tmp/reporte
